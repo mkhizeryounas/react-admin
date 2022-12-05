551e79e6527245fb01a0ff3ed0f8b291afbe3925
Muhammad Khizer Younas <khizer@fabric.inc>
2022-12-05 11:00:22 -0800

    feat: editer added

--- src/components/GlideGrid/index.jsx
+++ src/components/GlideGrid/index.jsx
@@ -1,124 +1,78 @@
 import { useCallback, useState } from 'react';
-import { DataEditor, GridCellKind } from '@glideapps/glide-data-grid';
+import { DataEditor } from '@glideapps/glide-data-grid';
 import '@glideapps/glide-data-grid/dist/index.css';
 import './style.scss';
 
+import {
+  useMockDataGenerator,
+  clearCell,
+  ColumnAddButton,
+  SimpleThemeWrapper,
+} from './utils';
+
 const GridTable = () => {
-  const singleRow = {
-    name: 'Hines Fowler ' + Math.random(),
-    company: 'BUZZNESS',
-    email: 'hinesfowler@buzzness.com',
-    phone: '+1 (869) 405-3127',
-    active: true,
-    image: 'https://picsum.photos/200/300',
-  };
-  const [data, setData] = useState(
-    new Array(2).fill(0).map((_, i) => singleRow)
-  );
+  const {
+    cols,
+    getCellContent,
+    setCellValueRaw,
+    setCellValue,
+    onColumnResize,
+  } = useMockDataGenerator(10, false);
 
-  const columns = [
-    {
-      title: 'Name',
-      id: 'name',
-      icon: 'headerString',
-    },
-    {
-      title: 'Company',
-      id: 'company',
-      icon: 'headerString',
-    },
-    {
-      title: 'Email',
-      id: 'email',
-      icon: 'headerString',
-    },
-    {
-      title: 'Phone',
-      id: 'phone',
-      icon: 'headerString',
-    },
-    {
-      title: 'Active',
-      id: 'active',
-      kind: GridCellKind.Boolean,
-      icon: 'headerBoolean',
-      readonly: false,
-    },
-    // {
-    //   title: 'Image',
-    //   id: 'image',
-    //   kind: GridCellKind.Image,
-    //   icon: 'headerImage',
-    // },
-  ];
+  const [numRows, setNumRows] = useState(50);
 
-  const getCellIds = () => {
-    return columns.map((e) => e.id);
-  };
-
-  const getContent = useCallback((cell) => {
-    const [col, row] = cell;
-    const dataRow = data[row];
-    // dumb but simple way to do this
-    const indexes = getCellIds();
-    const d = dataRow[indexes[col]];
-    const c = columns[col];
-    return {
-      kind: c.kind || GridCellKind.Text,
-      allowOverlay: true,
-      readonly: false,
-      displayData: d,
-      data: d,
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const onCellEdited = useCallback((cell, newValue) => {
-    // if (newValue.kind !== GridCellKind.Text) {
-    //   // we only have text cells, might as well just die here.
-    //   return;
-    // }
-    const indexes = getCellIds();
-    const [col, row] = cell;
-    const key = indexes[col];
-    const newData = [...data];
-    newData[row][key] = newValue.data;
-    console.log('newData', newData);
-    setData([...newData]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const onColumnResize = useCallback((_, width, i) => {
-    console.log(' columns[i]', columns[i], width);
-    columns[i]['width'] = width;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const onRowAppended = useCallback(() => {
+    const newRow = numRows;
+    for (let c = 0; c < cols.length; c++) {
+      const cell = getCellContent([c, newRow]);
+      setCellValueRaw([c, newRow], clearCell(cell));
+    }
+    setNumRows((cv) => cv + 1);
+  }, [cols.length, getCellContent, numRows, setCellValueRaw]);
 
   return (
     <div className='grid-table'>
       <div className='custom-grid-table'>
-        <DataEditor
-          getCellContent={getContent}
-          onCellEdited={onCellEdited}
-          columns={columns}
-          rows={data.length}
-          smoothScrollX={true}
-          smoothScrollY={true}
-          verticalBorder={true}
-          width='100%'
-          height='75vh'
-          rowMarkers='both'
-          isDraggable={true}
-          onColumnResizeEnd={onColumnResize}
-          onRowMoved={console.log}
-          onRowAppended={console.log}
-          trailingRowOptions={{
-            hint: 'New row...',
-            sticky: true,
-            tint: true,
-          }}
-        />
-        <div id='portal'></div>
+        <SimpleThemeWrapper>
+          <DataEditor
+            getCellContent={getCellContent}
+            onCellEdited={setCellValue}
+            columns={cols}
+            rows={numRows}
+            smoothScrollX={true}
+            smoothScrollY={true}
+            overscrollX={100}
+            scaleToRem={true}
+            theme={{
+              baseFontStyle: '0.8125rem',
+              headerFontStyle: '600 0.8125rem',
+              editorFontSize: '0.8125rem',
+            }}
+            verticalBorder={true}
+            width='100%'
+            height='75vh'
+            rowMarkers='both'
+            isDraggable={'cell'}
+            onColumnResize={onColumnResize}
+            onRowMoved={console.log}
+            onRowAppended={onRowAppended}
+            trailingRowOptions={{
+              hint: 'New row...',
+              sticky: true,
+              tint: true,
+            }}
+            rightElement={
+              <ColumnAddButton>
+                <button onClick={() => window.alert('Add a column!')}>+</button>
+              </ColumnAddButton>
+            }
+            rightElementProps={{
+              fill: false,
+              sticky: true,
+            }}
+          />
+          <div id='portal'></div>
+        </SimpleThemeWrapper>
       </div>
     </div>
   );
